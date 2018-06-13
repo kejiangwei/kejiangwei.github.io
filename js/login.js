79f302027a80a2de327c6ba2ac6d2ad3a8f2241f
kejiangwei <921543964@qq.com>
2018-06-14 00:11:05 +0800

    添加登录、注册、发布任务功能

--- js/login.js
+++ js/login.js
@@ -10,12 +10,15 @@ var login = (function() {
 	var aliyunUrl = 'http://annetinfo1.oss-cn-shenzhen.aliyuncs.com/'; //阿里云地址
 	var strUserName = ''; //用户名
 	var strUserPwdMd5 = ''; //加密后的密码
+	//	注册用户的对象数据
 	var registerInfo = {
 		"name": '',
 		"swjgid": '',
 		"tel": '',
 		"pass": ''
 	}
+	//	机关列表
+	var swjgPojoList = []
 
 	//	登录入口
 	function login() {
@@ -26,7 +29,7 @@ var login = (function() {
 		strUserPwdMd5 = md5Pwd;
 		var dataJson = {
 			"tel": userName,
-			"pass": strUserPwdMd5
+			"pass": userPwd
 		};
 		loginByInternet(dataJson);
 
@@ -37,25 +40,29 @@ var login = (function() {
 			"async": true,
 			"url": "http://" + hostUrl + "/fyproject/user/login",
 			"method": "POST",
-			"headers": {
-				"content-type": "application/json",
-				"cache-control": "no-cache"
-			},
-			"processData": false,
-			"data": JSON.stringify(dataJson)
+			"data": dataJson
 		}).done(function(response) {
 			console.log(response)
-			location.href = 'task.html';
+			if(response === '') {
+				mui.toast('账号密码错误');
+				return false
+			}else {
+				let userInfo = response;
+				console.log(userInfo);
+				localStorage.setItem("userInfo", JSON.stringify(userInfo));
+				localStorage.setItem("hostUrl",hostUrl)
+				location.href = 'task.html';
+			}
 		}).fail(function(error) {
 			console.log(error)
-			location.href = 'task.html';
 			mui.toast('网络错误');
 		});
 	}
+
 	//	注册入口
 	function register() {
 		registerInfo.name = $('#registerName').val();
-		registerInfo.swjgid = $('#registerSwjgid').text();
+		registerInfo.swjgid = $('#registerSwjgid').attr("data-name");
 		registerInfo.tel = $('#registerTel').val();
 		registerInfo.pass = $('#registerPwd').val();
 		if(registerInfo.pass != $('#sureRegisterPwd').val()) {
@@ -70,22 +77,78 @@ var login = (function() {
 				return false
 			}
 		}
+		registerInfo.userid = ''
+		console.log("******************注册信息")
+		console.log(registerInfo)
+		registerByInternet(registerInfo)
+	}
 
-		mui.toast('注册成功');
-		$(".loginBox").slideDown()
-		$(".registerBox").slideUp()
-
+	//	注册接口
+	function registerByInternet(dataJson) {
+		$.ajax({
+			"async": true,
+			"url": "http://" + hostUrl + "/fyproject/user/newuser",
+			"method": "POST",
+			"data": dataJson
+		}).done(function(response) {
+			console.log(response)
+			mui.toast('注册成功');
+			$(".loginBox").slideDown()
+			$(".registerBox").slideUp()
+			$('#loginName').val(dataJson.tel)
+			$('#loginPwd').val(dataJson.pass)
+		}).fail(function(error) {
+			console.log(error)
+			mui.toast('注册失败');
+		});
 	}
 
 	//	案件标识码登录
 	function code() {
+
 		location.href = 'task.html';
+	}
+
+	//	查找所有机关列表
+	function swjg_findallswjg() {
+		$.ajax({
+			"async": true,
+			"url": "http://" + hostUrl + "/fyproject/swjg/findallswjg",
+			"method": "POST",
+			"headers": {
+				"content-type": "application/json",
+				"cache-control": "no-cache"
+			},
+			"processData": false,
+		}).done(function(response) {
+			console.log(response)
+			for(let i in response.swjgPojoList) {
+				let obj = response.swjgPojoList[i]
+				swjgPojoList.push({
+					"value": obj.swjgid,
+					"text": obj.swjgmc,
+				})
+			}
+			console.log(swjgPojoList)
+			window.localStorage.setItem("swjgPojoList", JSON.stringify(swjgPojoList));
+			//			var crFilter = window.localStorage.getItem(getLocalKey("stMobileCrList"));  
+			//			crFilter = JSON.parse(crFilter);  
+
+		}).fail(function(error) {
+			console.log(error)
+			mui.toast('网络错误');
+		});
 	}
 
 	function OnLoadPage() {
 		console.log("××××××××××××进入登陆界面×××××××××××××××××")
+
+		setTimeout(function() {
+			$(".loader").hide();
+		}, 1000)
 		//		清空缓存
 		sessionStorage.clear();
+		localStorage.clear();
 		//		jqueryDom操作
 		//	打开注册界面
 		$(".registerAccount").click(function() {
@@ -110,38 +173,24 @@ var login = (function() {
 		//		注册选择机构
 		$("#selectMechanism").on("click", function() {
 			var picker = new mui.PopPicker();
-			picker.setData([{
-				value: "first",
-				text: "第一项"
-			}, {
-				value: "second",
-				text: "第一项"
-			}, {
-				value: "third",
-				text: "第三项"
-			}, {
-				value: "fourth",
-				text: "第四项"
-			}, {
-				value: "fifth",
-				text: "第五项"
-			}])
-			picker.pickers[0].setSelectedValue('fourth', 2000);
+			picker.setData(swjgPojoList)
+//			picker.pickers[0].setSelectedValue('fourth', 2000);
 			picker.show(function(SelectedItem) {
 				console.log(SelectedItem);
-				$("#registerSwjgid").text(SelectedItem[0].text)
+				$("#registerSwjgid").text(SelectedItem[0].text).attr("data-name",SelectedItem[0].value)
 			})
 		})
 
-		setTimeout(function() {
-			$(".loader").hide();
-		}, 1000)
+		//		加载所有机关列表
+		swjg_findallswjg()
+
 	}
 
 	return {
 		OnLoadPage: OnLoadPage,
 		login: login,
-		register: register
+		register: register,
+		code: code
 	}
 })();
 
